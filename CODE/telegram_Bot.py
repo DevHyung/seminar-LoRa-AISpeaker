1655c2bde3a1c3bd321e1294638711b9c0f0cd8a
DevHyung <khuphj@gmail.com>
2019-03-21 18:09:35 +0900

    Modify 텔레그램 오탈자

--- CODE/telegram_Bot.py
+++ CODE/telegram_Bot.py
@@ -8,13 +8,13 @@ import json
 TOKEN = 'INPUT_YOUR_TOKEN' 
 botUrl = "https://api.telegram.org/bot{}/".format(TOKEN)
 
-html = requests.get(Bot_URL+'getUpdates')
+html = requests.get(botUrl+'getUpdates')
 jsonStr = json.loads(html.text)
-chat_id = jsonStr['result'][0]['message']['chat']['id']
-chat_url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}".format(TOKEN, chat_id)
+chatId = jsonStr['result'][0]['message']['chat']['id']
+chatUrl = "https://api.telegram.org/bot{}/sendMessage?chat_id={}".format(TOKEN, chatId)
 
 # == Using
 if __name__=="__main__":
     while True:
-        _text = input(">>> 봇에게 보낼말 : ")
-        requests.get(chat_url + '&text='+_text)
+        text = input(">>> 봇에게 보낼말 : ")
+        requests.get(chatUrl + '&text='+text)
